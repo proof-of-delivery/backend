dc9c862693ef28144d655fb365da53b2654bcb6a
hemensol <hemensol14@yahoo.com>
2023-07-06 11:02:17 +0300

    make project api

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Pod_backend
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
+    config.api_only = true
   end
 end
