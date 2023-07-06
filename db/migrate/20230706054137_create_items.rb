fab4340b324dca101f9ff1e871f16c166715c787
hemensol <hemensol14@yahoo.com>
2023-07-06 09:29:46 +0300

    minor change

--- db/migrate/20230706054137_create_items.rb
+++ db/migrate/20230706054137_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.integer :position, null: false, unique: true
-      t.integer :item_no, null: false, unique: true
+      t.integer :position, null: false
+      t.integer :item_no, null: false
       t.string :description, null: false
-      t.string :supplier_item_id, null: false, unique: true
+      t.string :supplier_item_id, null: false
       t.integer :quantity, null: false
 
       t.timestamps
