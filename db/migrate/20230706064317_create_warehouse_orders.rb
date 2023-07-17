4277f758d482a4abcdb5d16ea13a4aafcf2a406b
hemensol <hemensol14@yahoo.com>
2023-07-17 17:49:22 +0300

    migration file update

--- db/migrate/20230706064317_create_warehouse_orders.rb
+++ db/migrate/20230706064317_create_warehouse_orders.rb
@@ -1,19 +1,12 @@
 class CreateWarehouseOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :warehouse_orders do |t|
-      t.string :customer_address
       t.string :doc_no, null: false
-      t.string :date
-      t.string :customer_id
-      t.string :assistance
-      t.string :email
       t.string :purchase_order_no, null: false
       t.string :name_of_ship
-      t.string :delivery_date
+      t.datetime :delivery_date
       t.string :delivery_address
-      t.string :voyage_no
-      t.string :weight_package
-      t.references :item, null: false, foreign_key: true
+      t.references :customer, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230717130154_add_columns_to_warehouse_order.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToWarehouseOrder < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :warehouse_orders, :customer, null: false, foreign_key: true
-    # add_reference :warehouse_orders, :user, null: false, foreign_key: true
-  end
-end
