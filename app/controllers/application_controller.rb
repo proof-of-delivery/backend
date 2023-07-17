23e87feb661eb9539f807cedca1d8a6d19c01e04
hemensol <hemensol14@yahoo.com>
2023-07-17 14:41:04 +0300

    Auth update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
+require 'jwt'
+
 class ApplicationController < ActionController::API
-    include JwtToken
+
 
     before_action :authenticate_user
 
@@ -9,12 +11,12 @@ class ApplicationController < ActionController::API
             header = request.headers['Authorization']
             header = header.split(' ').last if header
             begin
-                @decoded = JwtToken.decode(header)
+                @decoded = JWT.decode(header)
                 @current_user = User.find(@decoded[:user_id])
-            rescue ActiveRecord::RecordNotFound => e 
+            rescue ActiveRecord::RecordNotFound => e
                 render json: { errors: e.message }, status: :unauthorized
-            rescue JWT::DecodeError => e 
-                render json: { errors: e.message }, status: :unauthorized 
+            rescue JWT::DecodeError => e
+                render json: { errors: e.message }, status: :unauthorized
             end
         end
 end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,16 +1,32 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :authenticate_user
+
+    include JsonWebToken
+
+    before_action :authenticate_request
+
+
+
+
+
 
     # POST /auth/login
     def login
         @user = User.find_by_email(params[:email])
         if @user&.authenticate(params[:password])
             token = JwtToken.encode(user_id: @user.id)
-            time = Time.now + 24.hours.to_i 
+            time = Time.now + 24.hours.to_i
             render json: { token: token, exp: time.strftime ("m-%d-%Y %H: °M"),
                           username: @user.user_name }, status: :ok
-        else 
+        else
             render json: { error: 'unauthorized' }, status: :unauthorized
         end
     end
+
+    private
+    def authenticate_request
+
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        decoded = jwt_decode(header)
+        @current_user = User.find(decoded[:user_id])
 end
