a6b51e76286c0ace8c70c9267f2f6f4bb04f72ed
hemensol <hemensol14@yahoo.com>
2023-07-18 06:36:10 +0300

    model

---
+++ spec/models/warehouse_item_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe WarehouseItem, type: :model do
+  it 'has a valid factory' do
+    expect(create(:warehouse_item)).to be_valid
+  end
+
+  it {is_expected.to(validate_presence_of(:quantity))}
+  it{is_expected.to(belong_to(:item))}
+  it{is_expected.to(belong_to(:warehouse_order))}
+  it {is_expected.to(validate_numericality_of(:quantity))}
+  it {is_expected.to(validate_numericality_of(:requested_quantity))}
+  it {is_expected.to(validate_numericality_of(:confirmed_quantity))}
+  it {is_expected.to(validate_numericality_of(:total_confirmed_quantity))}
+  it {is_expected.to(validate_numericality_of(:total_requested_quantity))}
+  it {is_expected.to(validate_numericality_of(:picked_up_quantity))}
+end
