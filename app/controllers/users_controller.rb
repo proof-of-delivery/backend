f7a85ad8493e2ab3f82a94091e162d1eb57b20e2
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-07-20 20:54:02 +0300

    fixed skip before action method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action_skip :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :create
   before_action :find_user, except: %i[create index]
   
 
