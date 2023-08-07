2b930a1665b31338aa48d65613c4987f039e9936
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-08-07 04:52:08 +0300

    add after create

--- app/models/pickup_item.rb
+++ app/models/pickup_item.rb
@@ -5,8 +5,22 @@ class PickupItem < ApplicationRecord
 
   validates :quantity, presence: true
 
+  after_create :update_picked_up_quantity
+
+
   def self.ransackable_attributes(auth_object = nil)
     ['pickup_order_id', 'item_id', 'quantity', 'warehouse_item_id', 'id', 'created_at', 'updated_at']
+  end
+
+  private
+
+  def update_picked_up_quantity
+    ActiveRecord::Base.transaction do
+      warehouse_item.update!(
+        picked_up_quantity: warehouse_item.picked_up_quantity + quantity,
+        total_confirmed_quantity: warehouse_item.total_confirmed_quantity - quantity
+      )
+    end
   end
 
 end
