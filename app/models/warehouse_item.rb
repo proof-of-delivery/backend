230f1ed8386b70518dd2bdc38424b3841236f892
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-08-13 22:48:07 +0300

    added status to warehouse items

--- app/models/warehouse_item.rb
+++ app/models/warehouse_item.rb
@@ -1,9 +1,13 @@
 class WarehouseItem < ApplicationRecord
+  enum status: { waiting_approval: "waiting_approval", approved: "approved" }
+
+
   belongs_to :warehouse_order
   belongs_to :item
 
   validates :quantity, presence: true
   validates :quantity, :total_confirmed_quantity, :total_requested_quantity, :picked_up_quantity, numericality: true
+  
 
   def self.ransackable_attributes(auth_object = nil)
     ['warehouse_order_id', 'item_id', 'quantity','total_requested_quantity', 'total_confirmed_quantity', 'picked_up_quantity', 'id', 'created_at', 'updated_at']
